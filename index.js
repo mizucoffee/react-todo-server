f2a6d9f8a282a2afe90be2ea58bd4ee15006aecb
Ritsuki Goto <develop@mizucoffee.net>
2019-03-27 21:19:16 +0900

    [refactor] line

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express'),
     http = require('http').Server(app)
 
 const db = mongojs('localhost/react-todo', ['todo'])
-const s = http.listen(process.env.PORT || 3001, function () { })
+http.listen(process.env.PORT || 3001, function () { })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,37 +17,13 @@ app.get("/api/todo/:id", async (req, res) => {
     db.todo.findOne({ id: req.params.id }, (err, doc) => {
         if (doc == null) return res.json(createBody('not found'))
         delete doc._id
-        res.json(createBody(err, {
-            todo: doc
-        }))
+        res.json(createBody(err, { todo: doc }))
     })
 })
 
-app.get("/api/todo", async (req, res) => {
-    db.todo.find((err, doc) => {
-        res.json(createBody(err, {
-            todo_list: doc.map(i => {delete i._id; return i})
-        }))
-    })
-})
+app.get("/api/todo", async (req, res) => db.todo.find((err, doc) => res.json(createBody(err, { todo_list: doc.map(i => { delete i._id; return i }) }))))
+app.post("/api/todo", async (req, res) => db.todo.insert({ id: uniqid(), text: req.body.text }, (err, doc) => res.json(createBody(err, { id: doc.id }))))
+app.delete("/api/todo", async (req, res) => db.todo.remove({ id: req.body.id }, err => res.json(createBody(err, {}))))
+app.put("/api/todo", async (req, res) => db.todo.update({ id: req.body.id }, { $set: { "text": req.body.text } }, err => res.json(createBody(err, {}))))
 
-app.post("/api/todo", async (req, res) => {
-    const id = uniqid()
-    db.todo.insert({ id, text: req.body.text }, (err, doc) => res.json(createBody(err, { id: doc.id })))
-})
-
-app.delete("/api/todo", async (req, res) => {
-    db.todo.remove({ id: req.body.id }, err => res.json(createBody(err, {})))
-})
-
-app.put("/api/todo", async (req, res) => {
-    db.todo.update({ id: req.body.id }, { $set: { "text": req.body.text } }, err => res.json(createBody(err, {})))
-})
-
-function createBody(err, obj) {
-    return {
-        ...obj,
-        err,
-        ok: err == null
-    }
-}
+const createBody = (err, obj) => ({ ...obj, err, ok: err == null })
