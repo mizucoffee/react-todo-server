63b969a27d6b7389b5fc72adc8f944d9fb7b5526
Ritsuki Goto <develop@mizucoffee.net>
2019-03-28 15:00:56 +0900

    [add] add CROS

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ http.listen(process.env.PORT || 3001, function () { })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  
+    next();
+});
 
 app.get("/api/", async (req, res) => res.json(createBody(null, { version: 1 })))
 
@@ -23,7 +31,9 @@ app.get("/api/todo/:id", async (req, res) => {
 
 app.get("/api/todo", async (req, res) => db.todo.find((err, doc) => res.json(createBody(err, { todo_list: doc.map(i => { delete i._id; return i }) }))))
 app.post("/api/todo", async (req, res) => db.todo.insert({ id: uniqid(), text: req.body.text }, (err, doc) => res.json(createBody(err, { id: doc.id }))))
-app.delete("/api/todo", async (req, res) => db.todo.remove({ id: req.body.id }, err => res.json(createBody(err, {}))))
+app.delete("/api/todo", async (req, res) => {
+    db.todo.remove({ id: req.body.id }, err => res.json(createBody(err, {})))
+})
 app.put("/api/todo", async (req, res) => db.todo.update({ id: req.body.id }, { $set: { "text": req.body.text } }, err => res.json(createBody(err, {}))))
 
 const createBody = (err, obj) => ({ ...obj, err, ok: err == null })
